d29b1dc7f3b582ea42855c3c1cd3c5c42411c836
blake <ponda@PondaMashs-MacBook.local>
2018-03-20 15:36:20 +0000

    update 4

---
+++ js/bannerFade.js
@@ -0,0 +1,17 @@
+
+
+
+$(function () {
+  $('#banner').hide().fadeIn(1700);
+  if ('#banner').ready(){
+
+  }
+});
+  $('#row2').hide().fadeIn(4000);
+  $('#row3').hide().fadeIn(5000);
+  $('#row4').hide().fadeIn(6000);
+  $('#row5').hide().fadeIn(7000);
+  $('#row6').hide().fadeIn(8000);
+
+
+});
