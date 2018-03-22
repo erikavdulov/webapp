1f78237e80900aae485e7225571175bcecd788f9
blake <ponda@PondaMashs-MacBook.local>
2018-03-22 10:25:25 +0000

    banner

--- js/bannerFade.js
+++ js/bannerFade.js
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-
-
 
 $(function () {
   $('#banner').hide().fadeIn(2500);
 });
-=======
-
-
-
-$(function () {
-  $('#banner').hide().fadeIn(2500);
-});
->>>>>>> b8653877b195ee1dc892b640146c038a01b3425d
