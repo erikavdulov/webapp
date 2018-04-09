3794f755ab077a78ca4f25e8924660249190cf1f
dnas0856 <35962984+dnas0856@users.noreply.github.com>
2018-04-09 16:40:38 +0100

    Create prettyPrintJSON.js

---
+++ test/prettyPrintJSON.js
@@ -0,0 +1,31 @@
+$(function(){
+  //ready
+  alert ("document ready");
+
+  $('#searchform').submit(function(){
+    var searchterms=$("#searchterms").val();
+    //call search youtube function
+    getResultsFromIGDB(searchterms);
+    return false;
+  });
+});
+
+function getResultsFromIGDB(searchterms){
+  //call api using ajax
+  //build url for the request
+  var url ="http://www.omdbapi.com/?i=tt3896198&apikey=1b4f6264&s=" + searchterms;
+  //use jquery json shortcut
+  $.getJson(url,function(jsondata){
+    //handle the results
+    prettyprintJSON(jsondata);
+  });
+
+
+}
+
+
+function prettyprintJSON(jsondata){
+  //prints the json to the screen
+  var pretty= JSON.stringify(jsondata, null,4);
+  $('#resultsbox').append("<pre>"+pretty + "</pre>");
+}
