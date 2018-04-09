6e895e35ca05f00d9b0d77d3386bf2c58f840e63
dnas0856 <35962984+dnas0856@users.noreply.github.com>
2018-04-09 16:07:07 +0100

    Create prettyPrintJSON.js

---
+++ Team1/js/prettyPrintJSON.js
@@ -0,0 +1,31 @@
+$(function(){
+  //ready
+  alert ("document ready");
+
+  $('#searchform').submit(function(){
+    var searchterms=$("#searchterms").val();
+    //call search youtube function
+    getResultsFromIGDB(searchterms);
+    return false;
+  });
+});
+
+function getResultsFromIGDB(searchterms){
+  //call api using ajax
+  //build url for the request
+  var url ="http://api.igdb.com/?apikey=8fd263ee7124bbe3816cb5bba4e3c3ca&s=" + searchterms;
+  //use jquery json shortcut
+  $.getJson(url,function(jsondata){
+    //handle the results
+    prettyprintJSON(jsondata);
+  });
+
+
+}
+
+
+function prettyprintJSON(jsondata){
+  //prints the json to the screen
+  var pretty= JSON.stringify(jsondata, null,4);
+  $('#resultsbox').append("<pre>"+pretty + "</pre>");
+}
