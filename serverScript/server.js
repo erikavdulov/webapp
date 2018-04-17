328da21829fed7b8e4827346466ee80311b1870c
DOMINIK LEHOTAY (1705300) <1705300@rgu.ac.uk>
2018-04-17 15:28:05 +0100

    dom

--- serverScript/server.js
+++ serverScript/server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var currentdate = require('./mymodule');
+var currentdate = require('./modules');
 http.createServer(function (req, res) {
  res.writeHead(200, {'Content-Type': 'text/html'});
  res.write("The date and time are currently: " + currentdate.myDateTime());
