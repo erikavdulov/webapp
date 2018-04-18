20c3ab170bff354d22eac0d168d65fc9d5c64e66
dnas0856 <35962984+dnas0856@users.noreply.github.com>
2018-04-18 19:57:14 +0100

    Update finalScript.js

--- test/finalScript.js
+++ test/finalScript.js
@@ -13,7 +13,7 @@ $(function(){
 function getResultsFromOMDB(searchterms){
   //call api using ajax
   //build url for the request
-  var url ="http://www.omdbapi.com/?apikey=a6213293&s=" + searchterms;
+  var url ="https://api-endpoint.igdb.com/?apikey=8fd263ee7124bbe3816cb5bba4e3c3ca&s=" + searchterms;
   //use jquery json shortcut
   $.getJSON(url,function(jsondata){
     //handle the results
