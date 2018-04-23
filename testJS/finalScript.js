e2f9b1a0fba2e0fb1ffb1c4a2c79ffaf0726271f
DOMINIK LEHOTAY (1705300) <1705300@rgu.ac.uk>
2018-04-23 19:31:06 +0100

    dom

--- testJS/finalScript.js
+++ testJS/finalScript.js
@@ -1,3 +1,71 @@
+var apikey = "af44445c54798715ed796602012c4a33d507c499";
+var baseUrl = "http://www.giantbomb.com/api";
+
+// construct the uri with our apikey
+var GamesSearchUrl = baseUrl + '/search/?api_key=' + apikey + '&format=json';
+var query = "";
+
+$(document).ready(function() {
+
+  // send off the query
+  $('#form').submit(function(){
+    var userInput=$("#userInput").val();
+    //call search youtube function
+    getResultsFromOMDB(userInput);
+    return false;
+  });
+});
+
+function getResultsFromOMDB(userInput){
+  //call api using ajax
+  //build url for the request
+  var url ="https://www.giantbomb.com/api/search/?api_key=af44445c54798715ed796602012c4a33d507c499" + searchterms;
+  //use jquery json shortcut
+  $.getJSON(url,function(jsondata){
+    //handle the results
+    addResultTitles(jsondata);
+  });
+
+  function addResultTitles(jsondata){
+    var htmlstring = "";
+    for (var i=0; i<10; i++){
+      var title = jsondata.Search[i].Title;
+      htmlstring += "<li>" + title + "</li>";
+    }
+
+    $("#results2").html(htmlstring);
+  }
+
+
+/*
+$.ajax({
+    url: GamesSearchUrl + '&query=' + encodeURI(query),
+    dataType: "json",
+    success: searchCallback
+  });
+
+  function searched(){
+            var input = document.getElementById("userInput").value;
+            alert(input);
+            return(input);
+          }
+
+  function searchCallback(data) {
+    $('body').append('Found ' + data.total + ' results for ' + query);
+    var games = data.game;
+    $.each(games, function(index, game) {
+        $('body').append('<h1>' + game.name + '</h1>');
+        $('body').append('<p>' + game.description + '</p>');
+        $('body').append('<img src="' + game.posters.thumbnail + '" />');
+    });
+}
+});
+
+
+
+
+
+
 $(function(){
   //ready
   alert ("document ready");
@@ -41,3 +109,4 @@ function searched() {
         alert(input);
         return(query);
         }
+*/
