dd4c7a23afb366f6c9f5b0bcede7d07f2f9d379d
DOMINIK LEHOTAY (1705300) <1705300@rgu.ac.uk>
2018-04-26 09:56:54 +0100

    dom

--- prof/server.js
+++ prof/server.js
@@ -121,8 +121,6 @@ app.post('/dologin', function(req, res) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){res.redirect('/login');return}
-    if(req.session.loggedin){res.redirect('/index');return;
-        console.log('Already logged in')}
     //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
     if(result.login.password == pword){ req.session.loggedin = true; res.redirect('/index') }
     //otherwise send them back to login
